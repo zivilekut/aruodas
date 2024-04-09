9f0c7f61483ba8333d9e29898fa0b4a646d8979b
zivilekut <zivilekut@gmail.com>
2024-04-09 13:31:59 +0300

    selectMunicipality done

--- src/test/java/AruodasTest2.java
+++ src/test/java/AruodasTest2.java
@@ -1,10 +1,45 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.NoSuchElementException;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
 
 public class AruodasTest2 {
     ChromeDriver _globalDriver;
+
+    public WebElement snoozeUntilPresence(By by) {
+        WebDriverWait wait = new WebDriverWait(_globalDriver, Duration.ofSeconds(30));
+
+        return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+    }
+
+    public void selectMunicipality(String municipality) {
+        snoozeUntilPresence(By.xpath("//*[@id=\"newObjectForm\"]/ul/li[3]/span[1]/span")).click();
+        snoozeUntilPresence(By.xpath("//*[@id=\"regionDropdown\"]/li[1]/input")).sendKeys(municipality);
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (_globalDriver.findElement(By.xpath("//*[@id=\"regionDropdown\"]/li[64]")).isDisplayed()) {
+                System.out.println("You should write a longer name, because more than 1 option appears.");
+            } else {
+                snoozeUntilPresence(By.xpath("//*[@id=\"regionDropdown\"]/li[63]")).click();
+            }
+        } catch (NoSuchElementException e) {
+            snoozeUntilPresence(By.xpath("//*[@id=\"regionDropdown\"]/li[63]")).click();
+        }
+
+    }
 
     @BeforeTest
     public void setupWebDriver() {
@@ -13,6 +48,11 @@ public class AruodasTest2 {
         options.addArguments("--disable-notifications");
         _globalDriver = new ChromeDriver(options);
         _globalDriver.get("https://www.aruodas.lt/ideti-skelbima/?obj=1");
-        _globalDriver.findElement(By.id("onetrust-reject-all-handler")).click();
+        snoozeUntilPresence(By.id("onetrust-reject-all-handler")).click();
+    }
+
+    @Test //siauliai - zaliukiu k - danes g
+    public void test1() {
+        selectMunicipality("Šiauliai");
     }
 }
