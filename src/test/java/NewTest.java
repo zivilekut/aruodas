665990eeda1e81c378bc9c22def4d38634d89d95
zivilekut <zivilekut@gmail.com>
2024-04-09 13:37:12 +0300

    selectMunicipality done

--- src/test/java/NewTest.java
+++ src/test/java/NewTest.java
@@ -1,16 +1,16 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-import org.openqa.selenium.NoSuchElementException;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-public class AruodasTest2 {
+public class NewTest {
     ChromeDriver _globalDriver;
 
     public WebElement snoozeUntilPresence(By by) {
